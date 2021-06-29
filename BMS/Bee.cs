7095951141c8e8b9b73a9b69ca5284b5377b37d2
Serhii Melnykov <serhii.melnykov@outlook.com>
2021-06-29 20:52:19 +0300

    прототипы классов Bee и Hive

---
+++ BMS/Bee.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BMS
+{
+    // Чем сейчас занята пчела.
+    enum BeeState: byte
+    {
+        Idle,            // Свободна
+        FlyingToFlower,  // Летит на цветок
+        GatheringNectar, // Собирает нектар
+        ReturningToHive, // Возвращается в улей
+        MakingHoney,     // Производит мед
+        Retired          // Завершение работы
+    }
+
+    // Пчела.
+    class Bee
+    {
+        protected const double HoneyConsumed = 0.5;
+        protected const int MoveRate = 3; 
+        // минимально необходимое количество для сбора.
+        protected const double MinimumFlowerNectar = 1.5;
+        protected const int CareerSpan = 1000;
+        protected Point location;
+        public BeeState CurrentState { get; protected set; }
+
+        public int Age { get; protected set; } = 0;
+        public bool InsideHive { get; protected set; }
+        public double NectarCollected { get; protected set; }
+        public Point Location { get => location; }
+
+        private int ID;
+        private Flower destinationFlower;
+
+        public Bee(int id, Point location)
+        {
+            ID = id;
+            Age = 0;
+            this.location = location;
+            InsideHive = true;
+            CurrentState = BeeState.Idle;
+            destinationFlower = null;
+            NectarCollected = 0;
+        }
+
+        public void Go()
+        {
+            Age++;
+            switch (CurrentState)
+            {
+                case BeeState.Idle:
+                    if (Age > CareerSpan) CurrentState = BeeState.Retired;
+                    else /**/ ;
+                    break;
+
+                case BeeState.FlyingToFlower:
+                    break;
+
+                case BeeState.GatheringNectar:
+                    double nectar = destinationFlower.HarvestNectar();
+                    if (nectar > 0) NectarCollected += nectar;
+                    else CurrentState = BeeState.ReturningToHive;
+                    break;
+
+                case BeeState.ReturningToHive:
+                    if (!InsideHive) /**/;
+                    else /**/;
+                    break;
+
+                case BeeState.MakingHoney:
+                    if (NectarCollected < 0.5) {
+                        NectarCollected = 0;
+                        CurrentState = BeeState.Idle;
+                    } else {
+                        /**/
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private bool MoveTowardsLocation(Point destination)
+        {
+            if (Math.Abs(destination.X - Location.X) <= MoveRate &&
+                Math.Abs(destination.Y - Location.Y) <= MoveRate)
+            {
+                return true;
+            }
+
+            if (destination.X > Location.X)
+            {
+                (location).X += MoveRate;
+            } 
+            else if (destination.X < location.X)
+            {
+                location.X -= MoveRate;
+            }
+
+            if (destination.Y > location.Y)
+            {
+                location.Y += MoveRate;
+            }
+            else if (destination.Y < location.Y)
+            {
+                location.Y -= MoveRate;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ BMS/Hive.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BMS
+{
+    // Среда обитания пчел - УЛЕЙ
+    class Hive
+    {
+        // начальное количество пчел.
+        private const int initBeeCount = 6;
+
+        // начальное количество меда.
+        private const double initHoney = 3.2;
+
+        // максимальное количество меда.
+        private const double maxHoney = 15.0;
+
+        // коффициент переработки нектара в мед.
+        private const double nectarHoneyRatio = 0.25;
+
+        // максимальное количество пчел в улье.
+        private const int maxBeePopulation = 18;
+
+        // минимальное наличие меда для увеличения популяции.
+        private const double minHoneyForUpPopula = 4;
+
+        private static Random rand = new Random();
+
+        // количество меда в улье.
+        public double Honey { get; private set; }
+        private Point location;
+        private int beeCount;
+
+        // TODO: После тестирование перенести создание координат в конструктор.
+        private Dictionary<string, Point> places = new Dictionary<string, Point>
+        {
+            { "entrance", new Point(600, 100) },
+            {"nursery", new Point(95, 174) },
+            {"factory", new Point(157, 98) },
+            {"exit", new Point(194, 213) }
+        };
+
+        public void InitializeLocations(Point entrance, Point nursery, Point factory, Point exit) { 
+        
+        }
+
+        public Point GetLocation(string placeName)
+        {
+            if (places.ContainsKey(placeName)) return places[placeName];
+            throw new ArgumentException($"Unknow location: {placeName}");
+        }
+
+        public Hive()
+        {
+            Honey = initHoney;
+            for (int i = 0; i < initBeeCount; i++) AddBee();
+        }
+
+        public bool AddHoney(double nectar) => true;
+        public bool ConsumeHoney(double amount) => true;
+        private void AddBee() { }
+        public void Go() { }
+    }
+}
