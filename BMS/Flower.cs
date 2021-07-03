a9155e5a28e67f6d7b67834c1c9aa793480c208a
Serhii Melnykov <serhii.melnykov@outlook.com>
2021-07-03 16:19:50 +0300

    refactoring class Flower

--- BMS/Flower.cs
+++ BMS/Flower.cs
@@ -10,53 +10,57 @@ namespace BMS
     // Информация о цветке и его судьбе.
     class Flower
     {
-        // максимально возможное количество нектара.
-        protected const double MaxNectar = 5;
-
-        // нектар порождаемый по мере роста цветка.
-        protected const double NectarAddedPerTurn = 0.01;
-
+        // максимальное содержание нектара (вместимость).
+        protected const double MAX_NECTAR = 5.0;
+        // нектар порождаемый за один цикл.
+        protected const double NECTAR_ADDED_PER_TURN = 0.01;
         // нектар доступный для сбора за один цикл.
-        protected const double NectarGatheredPerTurn = 0.3;
+        protected const double NECTAR_GATHERED_PER_TURN = 0.3;
+        // минимальная продолжительность жизни.
+        protected const int LIFE_SPAN_MIN = 15_000;
+        // максимальная продолжительность жизни.
+        protected const int LIFE_SPANT_MAX = 30_000;
+        // начальное количество нектара.
+        protected const double INITIAL_NECTAR = 1.50;
+
+
+        // расположение цветка.
+        public Point Location { get; protected set; }
+        // текущий возраст.
+        public int Age { get; protected set; }
+        // цветок живой.
+        public bool Alive { get; protected set; }
+        // Количество доступного нектара.
+        public double Nectar { get; protected set; }
+        // Общее количество собранного c цветка нектара.
+        public double NectarHarvested { get; set; }
+        // продолжительность жизни.
+        protected readonly int lifespan;
+
 
         public Flower(Point location, Random rand)
         {
             Location = location;
             Age = 0;
             Alive = true;
-            Nectar = 1.50;
-            lifespan = rand.Next(15000, 30000);
+            Nectar = INITIAL_NECTAR;
+            lifespan = rand.Next(LIFE_SPAN_MIN, LIFE_SPANT_MAX);
             NectarHarvested = 0;
         }
 
-        // расположение цветка.
-        public Point Location { get; protected set; }
-
-        // текущий возраст.
-        public int Age { get; protected set; }
-
-        // цветок живой.
-        public bool Alive { get; protected set; }
-        
-        // Количество доступного нектара.
-        public double Nectar { get; protected set; }
-
-        // Общее количество собранного c цветка нектара.
-        public double NectarHarvested { get; protected set; }
-
-        // продолжительность жизни.
-        private readonly int lifespan;
 
         // отдача нектара пчеле.
         public double HarvestNectar() 
         {
-            if (this.Nectar >= NectarGatheredPerTurn)
+            if (NECTAR_GATHERED_PER_TURN > this.Nectar)
             {
-                this.Nectar -= NectarGatheredPerTurn;
-                this.NectarHarvested += NectarGatheredPerTurn;
-                return NectarGatheredPerTurn;
+                return 0;
+            } else
+            {
+                this.Nectar -= NECTAR_GATHERED_PER_TURN;
+                this.NectarHarvested += NECTAR_GATHERED_PER_TURN;
+                return NECTAR_GATHERED_PER_TURN;
             }
-            return 0;
         }
 
         // один жизненный цикл цветка.
@@ -64,13 +68,16 @@ namespace BMS
         {
             if (this.Alive)
             {
-                this.Age++;
-                if (this.Age >= this.lifespan) this.Alive = false;
-                if (this.Nectar >= MaxNectar) return;
-                this.Nectar = (this.Nectar + NectarAddedPerTurn) >= MaxNectar
-                    ? MaxNectar
-                    : this.Nectar + NectarAddedPerTurn;
-                // TODO: Что делать если цветок жив, но нектар достиг максимума?
+                if ((this.Age += 1) == this.lifespan)
+                {
+                    this.Alive = false;
+                }
+                if (this.Nectar < MAX_NECTAR)
+                {
+                    this.Nectar =
+                        (this.Nectar + NECTAR_ADDED_PER_TURN) >= MAX_NECTAR ? 
+                        MAX_NECTAR : this.Nectar + NECTAR_ADDED_PER_TURN;
+                }
             }
         }
     }
